96bf506164c4b4140396feaa5a372ad57d62ec5f
SvenPepermansFS <167076839+SvenPepermansFS@users.noreply.github.com>
2025-08-07 13:42:05 +0200

    typo fix

--- Mercurius.LAN.Web/Models/Matches/Placement.cs
+++ Mercurius.LAN.Web/Models/Matches/Placement.cs
@@ -7,6 +7,6 @@ namespace Mercurius.LAN.Web.Models.Matches
         public int Id { get; set; }
         public int GameId { get; set; }
         public int Place { get; set; }
-        public List<Participant> Participant { get; set; } = new();
+        public List<Participant> Participants { get; set; } = new();
     }
 }
